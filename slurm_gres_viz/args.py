5f5761853d837afcbaff41d17471be236a609635
Hyogun Lee <gunsbrother@khu.ac.kr>
2021-10-14 22:29:39 +0900

    Fix __init__
    The error is not beacuse of args itself but of __init__.py of the package captures the args while unittesting

--- slurm_gres_viz/args.py
+++ slurm_gres_viz/args.py
@@ -4,7 +4,7 @@ import argparse
 parser = argparse.ArgumentParser(description='SLURM Allocated GRES Visualizer')
 parser.add_argument('-i', '--index', action='store_true', 
                     help='Use Gres\' indices instead of stars(*)')
-parser.add_argument('-x', action='store_true', 
+parser.add_argument('-t', '--test', action='store_true', 
                     help='Test mode')
 args = parser.parse_args()
-args.test = args.x  # --test is used by pybuild
+
