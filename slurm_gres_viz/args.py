b8242a1f970f240d81de8e835d1c63cd7e7d95b8
HyogunGRAM <gunsbrother@naver.com>
2021-10-14 21:28:07 +0900

    Fix --test is used by pybuild

--- slurm_gres_viz/args.py
+++ slurm_gres_viz/args.py
@@ -4,6 +4,7 @@ import argparse
 parser = argparse.ArgumentParser(description='SLURM Allocated GRES Visualizer')
 parser.add_argument('-i', '--index', action='store_true', 
                     help='Use Gres\' indices instead of stars(*)')
-parser.add_argument('-t', '--test', action='store_true', 
+parser.add_argument('-t', action='store_true', 
                     help='Test mode')
 args = parser.parse_args()
+args.test = args.t  # --test is used by pybuild
